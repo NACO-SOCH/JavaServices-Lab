ceec9bf1bb60156fbe01f6829925254cb2c32b15
Thrisha Mohanan Geetha <u94639@ust-global.com>
2020-03-30 15:50:40 +0530

    fetch test results changes

--- src/main/java/gov/naco/soch/lab/controller/LabInchargeController.java
+++ src/main/java/gov/naco/soch/lab/controller/LabInchargeController.java
@@ -1,12 +1,17 @@
 package gov.naco.soch.lab.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import gov.naco.soch.dto.LabTestSampleBatchDto22;
 import gov.naco.soch.lab.service.LabInchargeService;
 
 @RestController
@@ -18,5 +23,14 @@ public class LabInchargeController {
 
 	@Autowired
 	private LabInchargeService labInchargeService;
+	
+
+	
+	/*
+	 * @GetMapping("/viewTestResults/{labId}") public List<LabTestSampleBatchDto22>
+	 * getTestResultDetails(@PathVariable(value = "labId") Long labId) {
+	 * logger.debug("getTestResultDetails is invoked!"); return
+	 * labInchargeService.getTestResultDetails(labId); }
+	 */
 
 }
