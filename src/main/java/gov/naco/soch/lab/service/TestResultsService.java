f46abd3a26405e090187d9fb4f4d7f3e5c1d1747
Thrisha Mohanan Geetha <u94639@ust-global.com>
2020-03-31 16:17:26 +0530

    TestResults fetch changes

--- src/main/java/gov/naco/soch/lab/service/TestResultsService.java
+++ src/main/java/gov/naco/soch/lab/service/TestResultsService.java
@@ -1,19 +1,14 @@
 package gov.naco.soch.lab.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import gov.naco.soch.entity.LabTestSample;
 import gov.naco.soch.entity.LabTestSampleBatch;
-import gov.naco.soch.entity.MasterResultStatus;
-import gov.naco.soch.exception.ServiceException;
 import gov.naco.soch.lab.dto.TestResultDto;
 import gov.naco.soch.lab.mapper.TestResultsMapperUtil;
 import gov.naco.soch.repository.LabTestSampleBatchRepository;
@@ -36,26 +31,6 @@ public class TestResultsService {
 		return labTestResultDtoList;
 	}
 
-	public boolean approveLabSampleResult(Long sampleId) {
 
-		boolean  isApproved=false;
-		Optional<LabTestSample>  labTestSample=labTestSampleRepository.findById(sampleId);
-		
-		if(labTestSample.isPresent())
-		{ 
-
-			MasterResultStatus masterResultStatus=new 	 MasterResultStatus(); 
-			//setting status to 3 which is Approved status
-			masterResultStatus.setId(new Long(3));
-			labTestSample.get().setMasterResultStatus(masterResultStatus);
-			isApproved=true; 
-			}
-		    else { 
-		    	throw new ServiceException(" SampleID not present",null, HttpStatus.BAD_REQUEST);
-
-			}
-
-		return isApproved; 
-	}
 
 }
