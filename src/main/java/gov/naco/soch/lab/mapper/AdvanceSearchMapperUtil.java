752b5b539e69cf8aa78b4f6db81f52e8c1ae36b0
jiji145999 <jiji.kakkoremmal@ust-global.com>
2020-07-11 17:17:08 +0530

    advance filter- record result

---
+++ src/main/java/gov/naco/soch/lab/mapper/AdvanceSearchMapperUtil.java
@@ -0,0 +1,83 @@
+package gov.naco.soch.lab.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class AdvanceSearchMapperUtil {
+	
+	public static List<String> queryCreaterForAdvanceSearchReceiveSampleList(Long labId, Map<String, String> searchValue) {
+		
+		String status = searchValue.get("status");
+		String facility = searchValue.get("facility");
+		String fromDate = searchValue.get("fromDate");
+		String toDate = searchValue.get("toDate");
+
+		List<String> searchQueryList = new ArrayList<>();
+
+		String searchQuery = "select * from soch.lab_test_sample_batch as lts where ";
+
+		if (status != null && !status.equals("")) {
+			Long sampleStatus = Long.valueOf(status);
+			searchQuery = searchQuery.concat(" lts.batch_status_id= " + sampleStatus + " and ");
+		}
+		if (facility != null && facility != "") {
+			Long byFacility = Long.valueOf(facility);
+			searchQuery = searchQuery.concat(" lts.artc_id= " + byFacility + " and ");
+		}
+		if (fromDate != null && fromDate != "" && (toDate == null || toDate == "")) {
+			searchQuery = searchQuery.concat(" cast(lts.dispatch_date as date) >= '" + fromDate + "' and ");
+		}
+		if (toDate != null && toDate != "" && (fromDate == null || fromDate == "")) {
+			searchQuery = searchQuery.concat(" cast(lts.dispatch_date as date) <= '" + toDate + "' and ");
+		}
+		
+		if (fromDate != null && fromDate != "" && toDate != null && toDate != "") {
+			searchQuery = searchQuery.concat(" cast(lts.dispatch_date as date) >= '" + fromDate + "' and cast(lts.dispatch_date as date) <= '" + toDate + "' and ");
+		}
+		
+		searchQuery = searchQuery.concat(" lts.is_delete = false and lts.lab_id= " + labId);
+
+		searchQueryList.add(searchQuery);
+
+		return searchQueryList;
+	}
+
+	public static List<String> queryCreaterForAdvanceSearchRecordResultsList(Long labId,Map<String, String> searchValue) {
+		
+		String status = searchValue.get("status");
+		String facility = searchValue.get("facility");
+		String fromDate = searchValue.get("fromDate");
+		String toDate = searchValue.get("toDate");
+
+		List<String> searchQueryList = new ArrayList<>();
+
+		String searchQuery = "select * from soch.lab_test_sample as lts where ";
+
+		if (status != null && status != "") {
+			Long sampleStatus = Long.valueOf(status);
+			searchQuery = searchQuery.concat(" lts.result_status_id= " + sampleStatus + " and ");
+		}
+		if (facility != null && facility != "") {
+			Long byFacility = Long.valueOf(facility);
+			searchQuery = searchQuery.concat(" lts.sample_collected_facility_id= " + byFacility + " and ");
+		}
+		if (fromDate != null && fromDate != "" && (toDate == null || toDate == "")) {
+			searchQuery = searchQuery.concat(" cast(lts.sample_received_date as date) >= '" + fromDate + "' and ");
+		}
+		if (toDate != null && toDate != "" && (fromDate == null || fromDate == "")) {
+			searchQuery = searchQuery.concat(" cast(lts.sample_received_date as date) <= '" + toDate + "' and ");
+		}
+		
+		if (fromDate != null && fromDate != "" && toDate != null && toDate != "") {
+			searchQuery = searchQuery.concat(" cast(lts.sample_received_date as date) >= '" + fromDate + "' and cast(lts.sample_received_date as date) <= '" + toDate + "' and ");
+		}
+		
+		searchQuery = searchQuery.concat(" lts.is_delete = false ");
+
+		searchQueryList.add(searchQuery);
+
+		return searchQueryList;
+	}
+
+}
--- src/main/java/gov/naco/soch/lab/mapper/ReceiveSamplesServiceMapperUtil.java
+++ src/main/java/gov/naco/soch/lab/mapper/ReceiveSamplesServiceMapperUtil.java
@@ -187,41 +187,4 @@ public class ReceiveSamplesServiceMapperUtil {
 		return labTestSampleDto;
 	}
 
-	public static List<String> queryCreaterForAdvanceSearch(Long labId, Map<String, String> searchValue) {
-		
-		String status = searchValue.get("status");
-		String facility = searchValue.get("facility");
-		String fromDate = searchValue.get("fromDate");
-		String toDate = searchValue.get("toDate");
-
-		List<String> searchQueryList = new ArrayList<>();
-
-		String searchQuery = "select * from soch.lab_test_sample_batch as lts where ";
-
-		if (status != null && status != " ") {
-			Long sampleStatus = Long.valueOf(status);
-			searchQuery = searchQuery.concat(" lts.batch_status_id= " + sampleStatus + " and ");
-		}
-		if (facility != null && facility != " ") {
-			Long byFacility = Long.valueOf(facility);
-			searchQuery = searchQuery.concat(" lts.artc_id= " + byFacility + " and ");
-		}
-		if (fromDate != null && fromDate != " " && (toDate == null || toDate == " ")) {
-			searchQuery = searchQuery.concat(" cast(lts.dispatch_date as date) >= '" + fromDate + "' and ");
-		}
-		if (toDate != null && toDate != " " && (fromDate == null || fromDate == " ")) {
-			searchQuery = searchQuery.concat(" cast(lts.dispatch_date as date) <= '" + toDate + "' and ");
-		}
-		
-		if (fromDate != null && fromDate != " " && toDate != null && toDate != " ") {
-			searchQuery = searchQuery.concat(" cast(lts.dispatch_date as date) >= '" + fromDate + "' and cast(lts.dispatch_date as date) <= '" + toDate + "' and ");
-		}
-		
-		searchQuery = searchQuery.concat(" lts.is_delete = false and lts.lab_id= " + labId);
-
-		searchQueryList.add(searchQuery);
-
-		return searchQueryList;
-	}
-
 }
