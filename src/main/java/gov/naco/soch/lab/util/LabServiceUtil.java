0de4c3f49412d2581c1d92d11dba45b09a520bc8
Anoob Kakkoth <anoob.kakkoth@ust-global.com>
2020-09-15 18:25:15 +0530

    Lab Migration Issue fix

--- src/main/java/gov/naco/soch/lab/util/LabServiceUtil.java
+++ src/main/java/gov/naco/soch/lab/util/LabServiceUtil.java
@@ -1,6 +1,7 @@
 package gov.naco.soch.lab.util;
 
 import gov.naco.soch.entity.Beneficiary;
+import gov.naco.soch.entity.LabBeneficiary;
 import gov.naco.soch.entity.UserMaster;
 
 public class LabServiceUtil {
@@ -13,6 +14,14 @@ public class LabServiceUtil {
 		return name.replaceAll("\\s+", " ");
 	}
 
+	public static String getBeneficiaryName(LabBeneficiary beneficiary) {
+		String firstName = beneficiary.getFirstName() != null ? beneficiary.getFirstName().concat(" ") : "";
+//		String middleName = beneficiary.getMiddleName() != null ? beneficiary.getMiddleName().concat(" ") : "";
+		String lastName = beneficiary.getLastName() != null ? beneficiary.getLastName() : "";
+		String name = firstName + lastName;
+		return name.replaceAll("\\s+", " ");
+	}
+	
 	public static String getBeneficiaryName(Beneficiary beneficiary) {
 		String firstName = beneficiary.getFirstName() != null ? beneficiary.getFirstName().concat(" ") : "";
 //		String middleName = beneficiary.getMiddleName() != null ? beneficiary.getMiddleName().concat(" ") : "";
